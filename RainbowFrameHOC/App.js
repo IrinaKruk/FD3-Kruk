1af96b4f64cc33aa06732c911c56dbc877eebd55
IrinaKruk <fatum2807@gmail.com>
2022-07-20 13:00:39 +0300

    new project RainbowFrameHOC

---
+++ RainbowFrameHOC/App.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DoubleButton from './components/DoubleButton';
+
+let colors = require('./colors.json');;
+
+const withRainbowFrame = (colors) => (Component) => {
+
+   class Rainbow extends React.Component {
+
+      render() {
+         let rainbow = <Component {...this.props} />;
+
+         for (let color of colors) {
+            rainbow = <div style={{ border: `solid 4px ${color}` }}>{rainbow}</div>
+         }
+         return rainbow;
+      }
+   }
+   return Rainbow;
+};
+
+let FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
+
+ReactDOM.render(
+   <FramedDoubleButton
+      caption1="я из лесу"
+      caption2="мороз"
+      cbPressed={num => alert(num)}>
+      вышел, был сильный
+   </FramedDoubleButton>
+   , document.getElementById('container')
+);
+
